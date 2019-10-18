a8289ec631f4bf3629d9666f71d853a20bf40cd6
Michiel Gerritsen <michiel@controlaltdelete.nl>
2019-10-18 20:25:14 +0200

    Feature: Allow multiple recipients

---
+++ src/Nova/Fields/EmailField.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ *    ______            __             __
+ *   / ____/___  ____  / /__________  / /
+ *  / /   / __ \/ __ \/ __/ ___/ __ \/ /
+ * / /___/ /_/ / / / / /_/ /  / /_/ / /
+ * \______________/_/\__/_/   \____/_/
+ *    /   |  / / /_
+ *   / /| | / / __/
+ *  / ___ |/ / /_
+ * /_/ _|||_/\__/ __     __
+ *    / __ \___  / /__  / /____
+ *   / / / / _ \/ / _ \/ __/ _ \
+ *  / /_/ /  __/ /  __/ /_/  __/
+ * /_____/\___/_/\___/\__/\___/
+ *
+ */
+
+namespace MichielGerritsen\LaravelNovaEmailOnEvent\Nova\Fields;
+
+use Laravel\Nova\Fields\Text;
+
+class EmailField extends Text
+{
+    public $component = 'email-on-event-email-field';
+}
